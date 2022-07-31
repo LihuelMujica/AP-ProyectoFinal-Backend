6ca50083c2aa95c15abf81af31c9f3847b050318
LihuelMujica <lihuelmujica@gmail.com>
2022-07-31 01:26:51 -0300

    Fix service update

--- src/main/java/com/argentinaprograma/portfolio/service/impl/IPortfolioServiceImpl.java
+++ src/main/java/com/argentinaprograma/portfolio/service/impl/IPortfolioServiceImpl.java
@@ -23,9 +23,17 @@ public class IPortfolioServiceImpl implements IPortfolioService {
 
     @Override
     public Portfolio update(Portfolio portfolio) throws ResourceNotFoundException {
+        Portfolio oldPortfolio = get();
         if(portfolio.getId() != null && portfolio.getId() != 1){
             throw new ResourceNotFoundException("Portfolio not found");
         }
+        if(portfolio.getId() == null) portfolio.setId(oldPortfolio.getId());
+        if(portfolio.getAbout()==null) portfolio.setAbout(oldPortfolio.getAbout());
+        if(portfolio.getCoverPicture()==null) portfolio.setCoverPicture(oldPortfolio.getCoverPicture());
+        if(portfolio.getDescription()==null) portfolio.setDescription(oldPortfolio.getDescription());
+        if(portfolio.getName()==null) portfolio.setName(oldPortfolio.getName());
+        if(portfolio.getProfilePicture()==null) portfolio.setProfilePicture(oldPortfolio.getProfilePicture());
+        portfolio.setId(1);
         return portfolioRepository.save(portfolio);
     }
 }
