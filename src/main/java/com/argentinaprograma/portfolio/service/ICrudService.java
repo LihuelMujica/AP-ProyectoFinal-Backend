c2466898e776887872b9671465a84a2f9210ab05
LihuelMujica <lihuelmujica@gmail.com>
2022-07-31 02:56:25 -0300

    Add delete method

--- src/main/java/com/argentinaprograma/portfolio/service/ICrudService.java
+++ src/main/java/com/argentinaprograma/portfolio/service/ICrudService.java
@@ -1,12 +1,14 @@
 package com.argentinaprograma.portfolio.service;
 
+import com.argentinaprograma.portfolio.exception.ResourceNotFoundException;
 import com.argentinaprograma.portfolio.persistence.entity.Education;
 
 import java.util.List;
 
 public interface ICrudService <Entity>{
     List<Entity> getAll();
-    Entity getById(int id);
+    Entity getById(int id) throws ResourceNotFoundException;
     Entity create(Entity entity);
     Entity update(Entity entity);
+    void delete(int id);
 }
