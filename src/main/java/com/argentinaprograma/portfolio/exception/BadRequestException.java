2b6357201c94f1b61ec353e71c64f6b608537c65
LihuelMujica <lihuelmujica@gmail.com>
2022-07-31 00:48:54 -0300

    Create basic exceptions

---
+++ src/main/java/com/argentinaprograma/portfolio/exception/BadRequestException.java
@@ -0,0 +1,7 @@
+package com.argentinaprograma.portfolio.exception;
+
+public class BadRequestException extends Exception {
+    public BadRequestException(String message){
+        super(message);
+    }
+}
---
+++ src/main/java/com/argentinaprograma/portfolio/exception/ResourceAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.argentinaprograma.portfolio.exception;
+
+public class ResourceAlreadyExistsException extends Exception{
+    public ResourceAlreadyExistsException(String message){
+        super(message);
+    }
+}
