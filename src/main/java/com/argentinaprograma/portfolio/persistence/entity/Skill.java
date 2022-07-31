480d4d881a1cd95b7d33924dad27aec55f2bac51
LihuelMujica <lihuelmujica@gmail.com>
2022-07-31 11:32:09 -0300

    Add getters, setters and constructors

--- src/main/java/com/argentinaprograma/portfolio/persistence/entity/Skill.java
+++ src/main/java/com/argentinaprograma/portfolio/persistence/entity/Skill.java
@@ -1,8 +1,17 @@
 package com.argentinaprograma.portfolio.persistence.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 
 @Entity(name = "skill")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Skill {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
