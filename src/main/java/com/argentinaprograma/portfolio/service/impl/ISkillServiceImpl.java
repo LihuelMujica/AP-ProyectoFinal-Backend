67824f27a367c819393031ccb437c38e537d6718
LihuelMujica <lihuelmujica@gmail.com>
2022-07-31 11:35:07 -0300

    Implement ISkillService

---
+++ src/main/java/com/argentinaprograma/portfolio/service/impl/ISkillServiceImpl.java
@@ -0,0 +1,53 @@
+package com.argentinaprograma.portfolio.service.impl;
+
+import com.argentinaprograma.portfolio.exception.ResourceAlreadyExistsException;
+import com.argentinaprograma.portfolio.exception.ResourceNotFoundException;
+import com.argentinaprograma.portfolio.persistence.entity.Skill;
+import com.argentinaprograma.portfolio.persistence.repository.SkillRepository;
+import com.argentinaprograma.portfolio.service.ISkillService;
+import com.argentinaprograma.portfolio.util.Utils;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class ISkillServiceImpl implements ISkillService {
+    private final SkillRepository repository;
+
+    @Autowired
+    public ISkillServiceImpl(SkillRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public List<Skill> getAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public Skill getById(int id) throws ResourceNotFoundException {
+        return repository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Skill not found")
+        );
+    }
+
+    @Override
+    public Skill create(Skill skill) throws ResourceAlreadyExistsException {
+        if(skill.getId() != null && repository.existsById(skill.getId())) throw new ResourceAlreadyExistsException("A skill with this id already exists");
+        return repository.save(skill);
+    }
+
+    @Override
+    public Skill update(Skill skill) throws ResourceNotFoundException {
+        Skill newSkill = getById(skill.getId());
+        Utils.copyProperties(skill, newSkill);
+        return repository.save(newSkill);
+    }
+
+    @Override
+    public void delete(int id) throws ResourceNotFoundException {
+        repository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Skill not found")
+        );
+        repository.deleteById(id);
+    }
+}
