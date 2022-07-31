dbbd45363db2912d20513d1fc6867d70737088bf
LihuelMujica <lihuelmujica@gmail.com>
2022-07-31 11:41:09 -0300

    Implement ISocialMediaService

---
+++ src/main/java/com/argentinaprograma/portfolio/service/impl/ISocialMediaServiceImpl.java
@@ -0,0 +1,54 @@
+package com.argentinaprograma.portfolio.service.impl;
+
+import com.argentinaprograma.portfolio.exception.ResourceAlreadyExistsException;
+import com.argentinaprograma.portfolio.exception.ResourceNotFoundException;
+import com.argentinaprograma.portfolio.persistence.entity.SocialMedia;
+import com.argentinaprograma.portfolio.persistence.repository.SocialMediaRepository;
+import com.argentinaprograma.portfolio.service.ISocialMediaService;
+import com.argentinaprograma.portfolio.util.Utils;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class ISocialMediaServiceImpl implements ISocialMediaService {
+    private final SocialMediaRepository repository;
+
+    @Autowired
+    public ISocialMediaServiceImpl(SocialMediaRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public List<SocialMedia> getAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public SocialMedia getById(int id) throws ResourceNotFoundException {
+        return repository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("SocialMedia not found")
+        );
+    }
+
+    @Override
+    public SocialMedia create(SocialMedia socialMedia) throws ResourceAlreadyExistsException {
+        Integer id = socialMedia.getId();
+        if(id != null && repository.existsById(id)) throw new ResourceAlreadyExistsException("A SocialMedia with this id already exists");
+        return repository.save(socialMedia);
+    }
+
+    @Override
+    public SocialMedia update(SocialMedia socialMedia) throws ResourceNotFoundException {
+        SocialMedia newSocialMedia = getById(socialMedia.getId());
+        Utils.copyProperties(socialMedia, newSocialMedia);
+        return repository.save(newSocialMedia);
+    }
+
+    @Override
+    public void delete(int id) throws ResourceNotFoundException {
+        repository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("SocialMedia not found")
+        );
+        repository.deleteById(id);
+    }
+}
