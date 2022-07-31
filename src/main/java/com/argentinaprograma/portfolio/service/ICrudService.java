189480d9cc851c751de6af96e212acd2fbe86267
LihuelMujica <lihuelmujica@gmail.com>
2022-07-31 03:01:53 -0300

    Add exceptions

--- src/main/java/com/argentinaprograma/portfolio/service/ICrudService.java
+++ src/main/java/com/argentinaprograma/portfolio/service/ICrudService.java
@@ -1,5 +1,6 @@
 package com.argentinaprograma.portfolio.service;
 
+import com.argentinaprograma.portfolio.exception.ResourceAlreadyExistsException;
 import com.argentinaprograma.portfolio.exception.ResourceNotFoundException;
 import com.argentinaprograma.portfolio.persistence.entity.Education;
 
@@ -8,7 +9,7 @@ import java.util.List;
 public interface ICrudService <Entity>{
     List<Entity> getAll();
     Entity getById(int id) throws ResourceNotFoundException;
-    Entity create(Entity entity);
-    Entity update(Entity entity);
-    void delete(int id);
+    Entity create(Entity entity) throws ResourceAlreadyExistsException;
+    Entity update(Entity entity) throws ResourceNotFoundException;
+    void delete(int id) throws ResourceNotFoundException;
 }
